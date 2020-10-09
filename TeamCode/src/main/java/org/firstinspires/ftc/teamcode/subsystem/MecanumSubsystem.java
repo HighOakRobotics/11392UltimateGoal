157be42793b78a67b4820d63f061050e2ea71ce8
Brian Lu <lu_haoqin@outlook.com>
2020-10-08 21:45:54 -0500

    stop wrapping and return instance field instead

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/MecanumSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/MecanumSubsystem.java
@@ -42,71 +42,7 @@ public class MecanumSubsystem extends Subsystem {
         mecanum.update();
     }
 
-    public void setDriveDST() {
-        mecanum.setDriveDST();
-    }
-
-    public void setDriveDST(DoubleSupplier drive, DoubleSupplier strafe, DoubleSupplier turn) {
-        mecanum.setDriveDST(drive, strafe, turn);
-    }
-
-    public TrajectoryBuilder trajectoryBuilder(Pose2d startPose) {
-        return mecanum.trajectoryBuilder(startPose);
-    }
-
-    public TrajectoryBuilder trajectoryBuilder(Pose2d startPose, boolean reversed) {
-        return mecanum.trajectoryBuilder(startPose, reversed);
-    }
-
-    public TrajectoryBuilder trajectoryBuilder(Pose2d startPose, double startHeading) {
-        return mecanum.trajectoryBuilder(startPose, startHeading);
-    }
-
-    public void turn(double angle) {
-        mecanum.turn(angle);
-    }
-
-    public void followTrajectory(Trajectory trajectory) {
-        mecanum.followTrajectory(trajectory);
-    }
-
-    public Pose2d getLastError() {
-        return mecanum.getLastError();
-    }
-
-    public boolean isBusy() {
-        return mecanum.isBusy();
-    }
-
-    public void setMode(DcMotor.RunMode runMode) {
-        mecanum.setMode(runMode);
-    }
-
-    public void setZeroPowerBehavior(DcMotor.ZeroPowerBehavior zeroPowerBehavior) {
-        mecanum.setZeroPowerBehavior(zeroPowerBehavior);
-    }
-
-    public PIDCoefficients getPIDCoefficients(DcMotor.RunMode runMode) {
-        return mecanum.getPIDCoefficients(runMode);
-    }
-
-    public void setPIDCoefficients(DcMotor.RunMode runMode, PIDCoefficients coefficients) {
-        mecanum.setPIDCoefficients(runMode, coefficients);
-    }
-
-    public List<Double> getWheelPositions() {
-        return mecanum.getWheelPositions();
-    }
-
-    public List<Double> getWheelVelocities() {
-        return mecanum.getWheelVelocities();
-    }
-
-    public void setMotorPowers(double v, double v1, double v2, double v3) {
-        mecanum.setMotorPowers(v, v1, v2, v3);
-    }
-
-    public double getRawExternalHeading() {
-        return mecanum.getRawExternalHeading();
+    public DriveTrainMecanum mecanum() {
+        return mecanum;
     }
 }
