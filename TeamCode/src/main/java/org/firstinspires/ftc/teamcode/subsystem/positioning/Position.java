c4468a1351c94b94c9cddbb6db10604faa9b2e53
Brian Lu <lu_haoqin@outlook.com>
2020-10-11 15:30:39 -0500

    modify Position for has

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/positioning/Position.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/positioning/Position.java
@@ -1,15 +1,24 @@
 package org.firstinspires.ftc.teamcode.subsystem.positioning;
 
 public class Position {
-    double xPosition; // inches
-    double yPosition; // inches
-    double heading; // radians
+    private boolean hasX;
+    private boolean hasY;
+    private boolean hasHeading;
+
+    private double xPosition; // inches
+    private double yPosition; // inches
+    private double heading; // radians
 
     public Position() {
-        this(0, 0, 0);
+        this(0.0, 0.0, 0.0);
     }
 
-    public Position(double x, double y, double heading) {
+    public Position(Double x, Double y, Double heading) {
+
+        hasX = x != null;
+        hasY = y != null;
+        hasHeading = heading != null;
+
         xPosition = x;
         yPosition = y;
         this.heading = heading;
@@ -19,23 +28,23 @@ public class Position {
         return xPosition;
     }
 
-    public void setxPosition(double xPosition) {
-        this.xPosition = xPosition;
-    }
-
     public double getyPosition() {
         return yPosition;
-    }
-
-    public void setyPosition(double yPosition) {
-        this.yPosition = yPosition;
     }
 
     public double getHeading() {
         return heading;
     }
 
-    public void setHeading(double heading) {
-        this.heading = heading;
+    public boolean hasX() {
+        return hasX;
+    }
+
+    public boolean hasY() {
+        return hasY;
+    }
+
+    public boolean hasHeading() {
+        return hasHeading;
     }
 }
